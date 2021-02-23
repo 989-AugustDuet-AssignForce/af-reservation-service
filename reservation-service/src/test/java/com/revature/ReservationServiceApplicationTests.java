66e491c8f481ce49dc529a2e0862b6f42305a682
Bryan.Wood <bryan.wood@lntinfotech.com>
2021-02-23 16:10:28 -0500

    start of test designs
    not actually writing the test as of yet, just creating them and figuring out what pieces are needed

--- reservation-service/src/test/java/com/revature/ReservationServiceApplicationTests.java
+++ reservation-service/src/test/java/com/revature/ReservationServiceApplicationTests.java
@@ -6,8 +6,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ReservationServiceApplicationTests {
 
+	
+	Reservation testRes;
+	
 	@Test
-	void contextLoads() {
+	void isValidReservation(Reservation r, Location rmId) {
+		//check to see if room is available for a  
+		//reservation during proposed time slot
 	}
+	
+	@Test
+	void isInvalidReservation(Reservation r, Location rmId) {
+		
+	}
+	@Test
+	void makeReservation(Reservation r, Location rmId) {
+		//see if making a reservation was successful
+	}
+	
+	@Test
+	void cancelReservation(Reservation r, Location rmId) {
+		//check to see if removal of reservation was successful
+	}
+	
+	
+	
 
 }
