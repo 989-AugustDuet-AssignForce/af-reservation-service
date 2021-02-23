41896b34b6e120ed64a4f423b43c0f98779dbdc8
Bryan.Wood <bryan.wood@lntinfotech.com>
2021-02-23 17:07:18 -0500

    end of day
    built a single test case, not sure if it is any good

--- reservation-service/src/test/java/com/revature/service/TestMakeReservationTimeSlot.java
+++ reservation-service/src/test/java/com/revature/service/TestMakeReservationTimeSlot.java
@@ -2,7 +2,11 @@ package com.revature.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Test;
+
+import com.revature.model.Reservation;
+
 
 class TestMakeReservationTimeSlot {
 
@@ -13,14 +17,27 @@ Reservation testRes;
     void isValidReservation(Reservation r, Location rmId) {
 		//check to see if room is available for a  
 		//reservation during proposed time slot
-
-	
+		
+		//make sure that room and reservations location id's all match 
+		AssertTrue(r.getBuildingId() == rmId.getBuildingId() 
+				&& r.getRoomId() == rmId.getRoomId() 
+				&& r.getLocationId() == rmId.getLocationId());
+		//		now assert that the room is available from start to stop times need lux api call??
+		//		AssertTrue(rmId.isAvailable(r.))
 	}
+	
 	
 	@Test
-	void isInvalidReservation(Reservation r, Location rmId) {
-		
+	void isInvalidReservation(Reservation r, Room rmId) {
+		//make sure it is a bad match
+		AssertFalse(r.getBuildingId() == rmId.getBuildingId() 
+		&& r.getRoomId() == rmId.getRoomId() 
+		&& r.getLocationId() == rmId.getLocationId());
+//		now assert that the room is not available from start to stop times need lux api call??
+//		AssertFalse(rmId.isAvailable(r.))
 	}
+	
+	
 	@Test
 	void makeReservation(Reservation r, Location rmId) {
 		//see if making a reservation was successful
