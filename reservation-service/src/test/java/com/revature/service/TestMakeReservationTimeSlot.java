ece152ce874a331910f6079e6d8201d1773faf45
Bryan.Wood <bryan.wood@lntinfotech.com>
2021-02-24 12:16:26 -0500

    Update TestMakeReservationTimeSlot.java

--- reservation-service/src/test/java/com/revature/service/TestMakeReservationTimeSlot.java
+++ reservation-service/src/test/java/com/revature/service/TestMakeReservationTimeSlot.java
@@ -4,47 +4,51 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.revature.model.Reservation;
 
+@SpringBootTest
+public class TestMakeReservationTimeSlot {
 
-class TestMakeReservationTimeSlot {
-
+	@Autowired
+	Reservation testRes;
+		
 	
-Reservation testRes;
 	
 	@Test
-    void isValidReservation(Reservation r, Location rmId) {
+    void isValidReservation(Reservation r) {
 		//check to see if room is available for a  
 		//reservation during proposed time slot
 		
 		//make sure that room and reservations location id's all match 
-		AssertTrue(r.getBuildingId() == rmId.getBuildingId() 
-				&& r.getRoomId() == rmId.getRoomId() 
-				&& r.getLocationId() == rmId.getLocationId());
+		AssertTrue(r.getBuildingId() == testRes.getBuildingId() 
+				&& r.getRoomId() == testRes.getRoomId() 
+				&& r.getLocationId() == testRes.getLocationId());
 		//		now assert that the room is available from start to stop times need lux api call??
-		//		AssertTrue(rmId.isAvailable(r.))
+		//		AssertTrue(testRes.isAvailable(r.))
 	}
 	
 	
 	@Test
-	void isInvalidReservation(Reservation r, Room rmId) {
+	void isInvalidReservation(Reservation r) {
 		//make sure it is a bad match
-		AssertFalse(r.getBuildingId() == rmId.getBuildingId() 
-		&& r.getRoomId() == rmId.getRoomId() 
-		&& r.getLocationId() == rmId.getLocationId());
+		AssertFalse(r.getBuildingId() == testRes.getBuildingId() 
+		&& r.getRoomId() == testRes.getRoomId() 
+		&& r.getLocationId() == testRes.getLocationId());
 //		now assert that the room is not available from start to stop times need lux api call??
-//		AssertFalse(rmId.isAvailable(r.))
+//		AssertFalse(testRes.isAvailable(r.))
 	}
 	
 	
 	@Test
-	void makeReservation(Reservation r, Location rmId) {
+	void makeReservation(Reservation r) {
 		//see if making a reservation was successful
 	}
 	
 	@Test
-	void cancelReservation(Reservation r, Location rmId) {
+	void cancelReservation(Reservation r) {
 		//check to see if removal of reservation was successful
 	}
 	
