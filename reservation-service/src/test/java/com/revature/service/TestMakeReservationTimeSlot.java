2d51f04df409d2d327025bea3595969c543f07b5
Bryan.Wood <bryan.wood@lntinfotech.com>
2021-02-24 14:37:32 -0500

    Update TestMakeReservationTimeSlot.java

--- reservation-service/src/test/java/com/revature/service/TestMakeReservationTimeSlot.java
+++ reservation-service/src/test/java/com/revature/service/TestMakeReservationTimeSlot.java
@@ -10,80 +10,128 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.revature.model.Reservation;
+import com.revature.model.RoomType;
 
 
 @SpringBootTest
 public class TestMakeReservationTimeSlot {
 
+	//  list of reservations from the repo, via the controller
 	@Autowired
-	ArrayList<Reservation> testResList; //invalid reservation that doesn't exist
-		
-	
-//	unneeded? commented out
-//	@Autowired
-//	RserevationRepository resRepo; //repository of reservations 
-	
+	ArrayList<Reservation> testResList; 
+
+	@Autowired
+	Reservation reservation;
+
+	//	unneeded? commented out
+	//	@Autowired
+	//	RserevationRepository resRepo; //repository of reservations 
+
 	//what should be used to interact with the reservation repository
 	@Autowired
-	ReservationController resCtrl; //reservation controller 
-	
+	ReservationController reserveControl; //reservation controller 
+
+	@BeforeClass
+	void createNewTestReservation() {
+		//hard coded reservation for now
+		reservation = new Reservation(1010010, 13, 7, 007, 1402, RoomType.PHYSICAL,
+				"BobTheBuilder" );
+	}
+
 	@BeforeClass
 	void createResList() {
-		testResList = resCtrl.getAllReservations();
+		//populate the list with the controller
+		testResList = reserveControl.getAllReservations();
 	}
-	
+
 	@Test
-    public void reservationIsListed(Reservation r) {
+	public void reservationIsListed() {
 		//check to see if reservation is present in the repo  
-		assertTrue(testResList.contains(r));
-	
+		assertTrue( testResList.contains( reservation ) );
+
 	}
-	
+
 	@Test
-	public void duplicateReservation(Reservation dupeRes) {
-		assertTrue(testResList.contains(dupeRes));
+	public void duplicateReservation() {
+		
+		//make a duplicate reservation and assert that it is already in the list
+		Reservation dupeReservation = reservation;
+		assertTrue( testResList.contains( dupeReservation ) );
 	}
-	
-	
-	
+
 	@Test
-	public void addReservationToRepository(Reservation r) {
+	public void isValidReservation() {
+
+		//does not exist yet
+		assertTrue( reserveControl.isValidReservation( reservation.getRoomId(), 
+				reservation.getStartDate(), reservation.getEndDate() ) );
+
+
+
+	}
+
+
+	@Test
+	public void addReservationToRepository() {
+		
 		//first affirm that the reservation does not exist in the repo
-		assertFalse(testResList.contains(r));
+		assertFalse( testResList.contains( reservation ) );
+
 		//add the reservation to the repo
-		resCtrl.addReservation(r);
+		reserveControl.addReservation( reservation );
+
 		//recreate Reservation List and confirm it is now listed
 		createResList();
-		assertTrue(testResList.contains(r));
+		assertTrue( testResList.contains( reservation ) );
 	}
-	
-	
-	
+
+
+
 	@Test
-	void reservationIsNotListed(Reservation r) {
+	void reservationIsNotListed() {
+		
 		//make sure it is a bad match
-		assertFalse(testResList.contains(r));
-		
+		assertFalse( testResList.contains( reservation ) );
+
 	}
-	
-	
+
+
 	@Test
-	void makeReservation(Reservation r) {
-		//see if making a reservation was successful
-		reservationIsNotListed(r);
-		addReservationToRepository(r);
+	void makeReservation() {
 		
+		//does reservation exist?
+		assertFalse( testResList.contains( reservation ) );
+		
+		//are the dates valid?
+		reserveControl.isValidReservation( reservation.getRoomId(), reservation.getReservationId() )
+		
+		//add the reservation
+		reserveControl.add(	reservation	);
+		
+		//repopulate list with current list from  database
+		createResList();
+		
+		//is the reservation in the most recent list?
+		assertTrue( testResList.contains( reservation ) );
+
 	}
-	
+
 	@Test
-	void cancelReservation(Reservation r) {
+	void cancelReservation() {
+		
+		//check to see if reservation exists
+		assertTrue( testResList.contains( reservation ) );
+		
+		//remove reservation
+		reserveControl.deleteReservation( reservation.getReservationId() );
+		createResList();
+		
 		//check to see if removal of reservation was successful
+		assertFalse( testResList.contains( reservation ) );
+		
 	}
-	
-	
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+
+
+
 
 }
