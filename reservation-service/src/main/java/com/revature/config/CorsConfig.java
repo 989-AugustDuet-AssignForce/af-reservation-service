1cef3c55d81b312cfcd915dd6295d45c5b0fd5c3
Joan Marin <johnm7770@gmail.com>
2021-03-12 11:53:27 -0500

    production!

--- reservation-service/src/main/java/com/revature/config/CorsConfig.java
+++ reservation-service/src/main/java/com/revature/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.revature.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,14 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
+	@Value("${revature.frontendBaseUrl}")
+	private String frontendBaseUrl;
+	
+	@Value("${revature.locationServiceBaseUrl}")
+	private String locationServiceBaseUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-        		.allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET","PUT", "POST", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("header1", "Content-Type", "Authorization")
-                .allowCredentials(true);
+        registry.addMapping( "/**" )
+        		.allowedOrigins( frontendBaseUrl, locationServiceBaseUrl )
+                .allowedMethods( "GET","PUT", "POST", "PATCH", "DELETE", "OPTIONS" )
+                .allowedHeaders( "header1", "Content-Type", "Authorization" )
+                .allowCredentials( true );
     }
 
 }
